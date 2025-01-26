7de05c83554a73a3e1655a2974e3b9e831d80557
Emma Bolmin <emmabolmin@yahoo.fr>
2025-01-26 15:01:50 +0100

    fix: fixed home page dimensions

--- bookclub-front/src/components/Home.vue
+++ bookclub-front/src/components/Home.vue
@@ -1,28 +1,32 @@
 <template>
-<div class="container mt-5">
+  <div class="container mt-5">
     <div v-if="isLoading" class="loading">
       <p>Assemblage de neurones en cours...</p>
     </div>
     <div v-else class="row">
       <div class="col-12 col-md-4 colonne_gauche">
         <h2 class="text-center">Présentation du Club</h2>
-        <p id="presentation_text">Bienvenue sur le site du club de lecture "Le Cercle de Lecture des Amies Imaginaires". Il est composé de membres fabuleuses, dont la noix de coco-autrice très connue Nina Fleur (Delacour) et Hei Heimma, une lectrice qui n'a pas froid aux yeux. Vous pourrez trouver sur cette application une liste exhaustive de toutes leurs lectures, de la fantasy (fantasie aussi) aux enquêtes. </p>
-        
+        <p id="presentation_text">Bienvenue sur le site du club de lecture "Le Cercle de Lecture des Amies Imaginaires".
+          Il est composé de membres fabuleuses, dont la noix de coco-autrice très connue Nina Fleur (Delacour) et Hei
+          Heimma, une lectrice qui n'a pas froid aux yeux. Vous pourrez trouver sur cette application une liste
+          exhaustive de toutes leurs lectures, de la fantasy (fantasie aussi) aux enquêtes. </p>
+
       </div>
       <div class="col-12 col-md-4 ">
         <CurrentBook :currentBook="currentBook" />
       </div>
       <div class="col-12 col-md-4 text-center colonne_droite">
-        <h2 >Résumé et Caractéristiques</h2>
+        <h2>Résumé et Caractéristiques</h2>
         <label for="bookSelector"><strong>Changer de livre :</strong></label>
         <select id="bookSelector" v-model="selectedBook" @change="changeBook">
           <option v-for="book in books" :key="book._id" :value="book._id">{{ book.title }}</option>
         </select>
         <p><strong>Auteur(e):</strong> {{ currentBook ? currentBook.author : '' }}</p>
         <p><strong>Genre :</strong> {{ currentBook ? currentBook.genre['title'] : '' }}</p>
-        <p><strong>Nombre de pages :</strong> {{ currentBook ? currentBook.pages : ''}}</p>
-        <p id="resume_text"><strong>Résumé :</strong> {{ currentBook ? currentBook.summary : '' }}</p>
-        
+        <p><strong>Nombre de pages :</strong> {{ currentBook ? currentBook.pages : '' }}</p>
+        <p id="resume_text" class="scrollable"><strong>Résumé :</strong> {{ currentBook ? currentBook.summary : '' }}
+        </p>
+
       </div>
     </div>
   </div>
@@ -33,58 +37,57 @@ import CurrentBook from './CurrentBook.vue';
 
 
 export default {
-    name: "MyHome",
-    components: {
-        CurrentBook
-    },
-    data() {
-        return {
-            selectedBook: null,
-            books: [
-                
-            ],
-            currentBook: null,
-            isLoading: true
+  name: "MyHome",
+  components: {
+    CurrentBook
+  },
+  data() {
+    return {
+      selectedBook: null,
+      books: [
+
+      ],
+      currentBook: null,
+      isLoading: true
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    async getBooks() {
+      try {
+        const response = await fetch('https://bookclub-api.vercel.app/api/books');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-    },
-    computed: {
-        
-    },
-    methods: {
-      async getBooks() {
-            try {
-                const response = await fetch('https://bookclub-api.vercel.app/api/books');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                this.books = await response.json();
-                if (this.books.length > 0) {
-                    this.selectedBook = this.books[0]._id;
-                    this.changeBook();
-                }
-            } catch (error) {
-                console.error('Erreur lors de la récupération des livres:', error);
-            } finally {
-                this.isLoading = false;
-            }
-        },
-        changeBook() {
-            this.currentBook = this.books.find(book => book._id === this.selectedBook);
-            }
-        },
-      mounted() {
-          
-          this.getBooks();
-      
+        this.books = await response.json();
+        if (this.books.length > 0) {
+          this.selectedBook = this.books[0]._id;
+          this.changeBook();
+        }
+      } catch (error) {
+        console.error('Erreur lors de la récupération des livres:', error);
+      } finally {
+        this.isLoading = false;
       }
-    
+    },
+    changeBook() {
+      this.currentBook = this.books.find(book => book._id === this.selectedBook);
+    }
+  },
+  mounted() {
+
+    this.getBooks();
+
+  }
+
 }
 
 </script>
 
 
 <style scoped>
-
 .container {
   background-color: rgba(255, 255, 255, 0.80);
   border-radius: 10px;
@@ -120,11 +123,18 @@ export default {
 }
 
 #resume_text {
-    width: 100%;
-    overflow-wrap: break-word;
-  }
+  width: 100%;
+  overflow-wrap: break-word;
+}
+
+.scrollable {
+  max-height: 160px;
+  /* Ajustez cette valeur selon vos besoins */
+  overflow-y: auto;
+}
 
 @media (max-width: 768px) {
+
   /* .row {
     overflow-y: auto;
     max-height: 50vh;
@@ -133,10 +143,17 @@ export default {
     width: 100%;
     overflow-wrap: break-word;
   }
+
   #presentation_text {
-      text-align: justify;
-      font-size: large;
-      overflow-wrap: break-word;
+    text-align: justify;
+    font-size: medium;
+    overflow-wrap: break-word;
+  }
+
+  .scrollable {
+    max-height: 50px;
+    /* Ajustez cette valeur selon vos besoins */
+    overflow-y: scroll;
   }
 }
 </style>
--- bookclub-front/src/views/HomeView.vue
+++ bookclub-front/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
-   
-       <div class="home">
+
+    <div class="home">
         <Home />
-       </div>
-    
+    </div>
+
 </template>
 
 
@@ -23,7 +23,6 @@ export default {
 
 
 <style scoped>
-
 /* .home {
     display: flex;
     justify-content: center;
@@ -34,16 +33,16 @@ export default {
 } */
 
 .home {
-  width: 100%;
-  height:115vh;
-  background: url("../assets/banniere_CLAI.png");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-color: #A3454C;
-  background-position: center center;
-  overflow-y: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    height: 115vh;
+    background: url("../assets/banniere_CLAI.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color: #A3454C;
+    background-position: center center;
+    /* overflow-y: hidden; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
