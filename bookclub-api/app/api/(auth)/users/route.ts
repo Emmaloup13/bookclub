1439287908566ca6a820b82ab45785d440ac2ab4
Emma Bolmin <emmabolmin@yahoo.fr>
2025-02-02 11:11:48 +0100

    fix: api populate user after patch

--- bookclub-api/app/api/(auth)/users/route.ts
+++ bookclub-api/app/api/(auth)/users/route.ts
@@ -96,7 +96,7 @@ export const PATCH = async (request: Request) => {
             { _id: userId },
             { username: newUsername, imageUrl: newImageUrl, genres: newGenres, readers: newReaders, books: newBooks, comments: newComments },
             { new: true }
-        );
+        ).populate('genres books comments');
 
         await Book.updateMany({ "_id": { $in: newBooks } }, { $push: { readers: userId } });
         await Comment.updateMany({ "_id": { $in: newComments } }, { $push: { author: userId } });
