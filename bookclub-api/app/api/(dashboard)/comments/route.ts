02253769159bd5aa723ba508ca3556ea8ca41a40
Emma Bolmin <emmabolmin@yahoo.fr>
2025-02-09 22:26:23 +0100

    fix: added populate to post and patch of comment

--- bookclub-api/app/api/(dashboard)/comments/route.ts
+++ bookclub-api/app/api/(dashboard)/comments/route.ts
@@ -29,6 +29,7 @@ export const POST = async (request: Request) => {
         const body = await request.json();
         await connect();
         const newComment = new Comment(body);
+        newComment.populate([{ path: 'author', model: User }, { path: 'book', model: Book }]);
         await newComment.save();
 
         await User.updateOne({ "_id": newComment.author }, { $push: { comments: newComment._id } });
@@ -73,7 +74,7 @@ export const PATCH = async (request: Request) => {
             { _id: commentId },
             { text: newText },
             { new: true }
-        );
+        ).populate([{ path: 'author', model: User }, { path: 'book', model: Book }]);
 
         if (!updatedComment) {
             return new NextResponse(
